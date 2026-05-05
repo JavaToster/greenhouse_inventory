7f015809492239de665c68832521fee506527ed8
JavaToster <kamil.gizatullin.03@gmail.com>
2026-05-05 12:41:26 +0300

    First commit

---
+++ src/main/java/com/example/inventory/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.inventory.configuration.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http){
+        http
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .;
+
+        return http.build();
+    }
+}
